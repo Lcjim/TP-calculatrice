ad9eb86dbea95761c00f3ef0d10b51cd376719b2
Lcjim <diamxridot@gmail.com>
2023-11-28 11:55:45 +0100

    fini

--- main.c
+++ main.c
@@ -67,7 +67,7 @@ int main(){
             } else {
                 printf("Erreur de calcul ! On ne peut pas diviser par zéro.\n");
             }
-        }else if (nature == '^'){
+        }else if (nature == '^'){       /* on mets ^ comme nature pour faire la puissance du nombre  de base et le exponent */
             printf(" choisir le premier nombre sera la base.\n");
             scanf("%d",&number1);
             getchar();
@@ -76,21 +76,18 @@ int main(){
             getchar();
             calcul=pow(number1,number2);
             printf("le résultat de %d ** %d  est: %d \n",number1,number2, calcul);
-            if (number1 && number2 == 0) {
-                printf("Erreur de cacul!\n,on peut multiplier par zero");
 
-            }
-        }else if (nature == 's') {
+        }else if (nature == 's') {     /* on mets s comme nature pour faire la racine du premier nombre */
             printf(" choisir le premier nombre sera la base.\n");
             scanf("%d", &number1);
             getchar();
-            if(number1>1){
+            if(number1>1){          /* la racine du premier nombre doit être positive donc supérieur à 1 */
                 calcul = sqrt(number1);
                 printf("le résultat de racine( %d ) est: %d \n", number1, calcul);
-            }else if (number1 <0 ){
+            }else if (number1 <0 ){       /* la racine du premier est négative, donner aucun résultat */
                 printf("Opérateur non reconnu,La racine carrée d'un nombre négatif n'existe pas.\n");
             }
-        }else if (nature== '%') {
+        }else if (nature== '%') {        /* on mets % comme nature pour le pourcentage du premier nombre et le second */
             printf("choisir un nombre  .\n");
             scanf("%d", &number1);
             getchar();
@@ -105,10 +102,6 @@ int main(){
         getchar();
     }
     printf("vous avez sortie de la calculatrice");
-
-
-
-
 
     return 0;
 }
