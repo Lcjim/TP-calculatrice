9157e0579824ae5b678ae3cf51fd108991aa72c3
Lcjim <diamxridot@gmail.com>
2023-11-28 11:39:32 +0100

    fini

---
+++ main.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+
+int main(){
+    char choix;
+    char nature;
+    int number1;
+    int number2;
+    int calcul;
+
+
+    printf("pour continuer à calculer appuyer [0:oui, 1:non]");
+    scanf("%d",&choix);
+    getchar();
+    while(choix==0){
+        printf(" choisir la nature de calcul\n"
+               "addition= +, soustraction = -, multiplication = * , division = / \n"
+               ", puissance = ^ , racine carrée = s, et pourcentage = %.\n");
+        scanf("%c",&nature);
+        if (nature== '+'){
+            printf(" choisir le premier nombre.\n");
+            scanf("%d",&number1);
+            getchar();
+            printf(" choisir le second nombre.\n");
+            scanf("%d",&number2);
+            getchar();
+            calcul=number1 + number2;
+
+            printf("le résultat de %d + %d est: %d  \n",number1,number2, calcul);
+
+        }else if (nature== '-'){
+            printf(" choisir le premier nombre.\n");
+            scanf("%d",&number1);
+            getchar();
+            printf(" choisir le second nombre.\n");
+            scanf("%d",&number2);
+            getchar();
+            calcul=number1 - number2;
+            printf("le résultat de %d - %d est: %d  \n",number1,number2, calcul);
+
+        }else if (nature== '*') {
+            printf(" choisir le premier nombre.\n");
+            scanf("%d", &number1);
+            getchar();
+            printf(" choisir le second nombre.\n");
+            scanf("%d", &number2);
+            getchar();
+            calcul = number1 * number2;
+            printf("le résultat de %d x %d est: %d \n", number1, number2, calcul);
+            if (number1 ||number2 == 0) {
+                printf("Erreur de cacul!\n,on peut multiplier par zero");
+
+            }
+        }else if (nature=='/'){
+            printf(" choisir le premier nombre.\n");
+            scanf("%d",&number1);
+            getchar();
+            printf(" choisir le second nombre.\n");
+            scanf("%d",&number2);
+            getchar();
+
+            if (number1 && number2 != 0) {
+                calcul = number1 / number2;
+                printf("Le résultat de %d / %d est : %d\n", number1, number2, calcul);
+            } else {
+                printf("Erreur de calcul ! On ne peut pas diviser par zéro.\n");
+            }break;
+        }else if (nature == '^'){
+            printf(" choisir le premier nombre sera la base.\n");
+            scanf("%d",&number1);
+            getchar();
+            printf(" choisir le second nombre sera l'exponent.\n");
+            scanf("%d",&number2);
+            getchar();
+            calcul=pow(number1,number2);
+            printf("le résultat de %d ** %d  est: %d \n",number1,number2, calcul);
+            if (number1 && number2 == 0) {
+                printf("Erreur de cacul!\n,on peut multiplier par zero");
+
+            }
+        }else if (nature == 's') {
+            printf(" choisir le premier nombre sera la base.\n");
+            scanf("%d", &number1);
+            getchar();
+            if(number1>1){
+                calcul = sqrt(number1);
+                printf("le résultat de racine( %d ) est: %d \n", number1, calcul);
+            }else if (number1 <0 ){
+                printf("Opérateur non reconnu,La racine carrée d'un nombre négatif n'existe pas.\n");
+            }
+        }else if (nature== '%') {
+            printf("choisir un nombre  .\n");
+            scanf("%d", &number1);
+            getchar();
+            printf("le second sert de pourcentage.\n");
+            scanf("%d", &number2);
+            getchar();
+            calcul =  number1* number2/100;
+            printf("le résultat de %d x %d/100 est: %d \n", number1, number2, calcul);
+        }
+        printf("pour continuer à calculer appuyer [0:oui, 1:non]");
+        scanf("%d",&choix);
+        getchar();
+    }
+    printf("vous avez sortie de la calculatrice");
+
+
+
+
+
+    return 0;
+}
